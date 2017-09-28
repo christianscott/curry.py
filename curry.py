210fcb9834156b248f695083e14dc30f8d87acbf
chrfrasco <christianfscott@gmail.com>
2017-09-28 14:29:12 +1300

    Add more tests

--- curry.py
+++ curry.py
@@ -21,19 +21,38 @@ def curry(fun):
 class CurryTest(unittest.TestCase):
     def test_two_args(self):
         add = curry(lambda a, b: a + b)
-        self.assertEqual(2, add(1)(1))
+        self.assertEqual(3, add(1)(2))
 
     def test_three_args(self):
         add3 = curry(lambda a, b, c: a + b + c)
-        self.assertEqual(3, add3(1)(1)(1))
+        self.assertEqual(6, add3(1)(2)(3))
+
+    def test_mutable_args(self):
+        concat = curry(lambda a, b: a.extend(b))
+        self.assertEqual([1, 2, 3, 4], concat([1, 2])([3, 4]))
+
+    def test_builtin(self):
+        add_1_to_each = curry(map)(lambda x: x + 1)
+        self.assertEqual([2, 3, 4, 5], 
+                         list(add_1_to_each([1, 2, 3, 4])))
 
     def test_positional_kwargs(self):
         add_default = curry(lambda a, b=10: a + b)
-        self.assertEqual(2, add_default(1)(1))
+        self.assertEqual(3, add_default(1)(2))
 
     def test_kwargs(self):
         add_default = curry(lambda a, b=10: a + b, default=True)
         self.assertEqual(12, add_default(2))
+
+    def test_preserve_name(self):
+        def add(a, b): return a + b
+        add = curry(add)
+        self.assertEqual('add', add)
+
+    def test_preserve_name(self):
+        def add(a, b): return a + b
+        add = curry(add)
+        self.assertEqual('add', add.__name__)
 
 if __name__ == '__main__':
     unittest.main()
