592780cc8e0ce6431e70444300c5a60875c2782a
chrfrasco <christianfscott@gmail.com>
2017-10-11 17:04:45 +1300

    Refactor tests

--- test_curry.py
+++ test_curry.py
@@ -1,29 +1,48 @@
+import math
 import unittest
 from curry import curry
 
+def func(a, b, *, c, d):
+    return (a, b), dict(c=c, d=d)
 
-class CurryTest(unittest.TestCase):
+class TestCurry(unittest.TestCase):
 
-    def test_one_args(self):
-        id = curry(lambda a: a)
-        self.assertEqual(1, id(1))
+    def test_basic_examples(self):
+        f = curry(func)
+        self.assertEqual(f(1)(2)(c=10)(d=20),
+                         ((1, 2), dict(c=10, d=20)))
 
-    def test_two_args(self):
-        add = curry(lambda a, b: a + b)
-        self.assertEqual(3, add(1)(2))
+    def test_builtins(self):
+        f = curry(map)(lambda x: x + 1)
+        self.assertEqual(list(f([1, 2, 3, 4])),
+                                [2, 3, 4, 5])
 
-    def test_three_args(self):
-        add3 = curry(lambda a, b, c: a + b + c)
-        self.assertEqual(6, add3(1)(2)(3))
+        f = curry(math.pow)(2)
+        self.assertAlmostEqual(f(4), 16)
+
+        f = curry(''.join)
+        self.assertEqual('123', f(map(str, [1, 2, 3])).call_with_arguments())
+
+    def test_attributes(self):
+        f = curry(func)(1)(2)(c=10)
+
+        self.assertEqual(f.fun, func)
+        self.assertEqual(f.args, (1, 2))
+        self.assertEqual(f.kwargs, dict(c=10))
+
     def test_argument_check(self):
         self.assertRaises(TypeError, curry, 1)
 
     def test_args_dont_persist(self):
-        add = curry(lambda a, b: a + b)
-        add1 = add(1)
-        add2 = add(2)
-        self.assertEqual(2, add1(1))
-        self.assertEqual(3, add2(1))
+        curried_func = curry(func)
+
+        f = curried_func(1)(2)(c=10)
+        g = curried_func('a')('b')(c='c')
+
+        self.assertEqual(f(d=20),
+                         ((1, 2), dict(c=10, d=20)))
+        self.assertEqual(g(d='d'),
+                         (('a', 'b'), dict(c='c', d='d')))
 
     def test_args_dont_persist_after_first(self):
         factory = curry(lambda a, b, c: None)
@@ -56,19 +75,6 @@ class CurryTest(unittest.TestCase):
     def test_positional_kwargs(self):
         add_default = curry(lambda a, b=10: a + b)
         self.assertEqual(3, add_default(1)(2))
-
-    def test_kwargs(self):
-        @curry
-        def add(a, *, b):
-            return a + b
-        self.assertEqual(12, add(2)(b=10))
-
-    def test_preserve_name(self):
-        def add(a, b):
-            return a + b
-        add = curry(add)
-        self.assertEqual('add', add.__name__)
-        self.assertEqual('add', add(1).__name__)
 
 if __name__ == '__main__':
     unittest.main()
