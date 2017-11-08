f1c78c699dc63252823d6ba848eb9c3233cba0c7
Christian Scott <christianfscott@gmail.com>
2017-11-09 10:35:15 +1300

    Pull `have_enough_args` out of curry function
    Adding an argument is a fair price to pay to pull the function
    definition to the top level. It's (I think) a lot clearer this way;
    the extra definition inside of `curry` was visual noise that didn;t
    contribute to understanding the function.

--- curry.py
+++ curry.py
@@ -3,16 +3,13 @@
 from functools import wraps
 from inspect import signature, isbuiltin, isclass
 
+
 def curry(func, args=None, kwargs=None):
     if not callable(func):
         raise TypeError('first argument must be callable')
 
     args = args if args is not None else tuple()
     kwargs = kwargs if kwargs is not None else dict()
-
-    def have_enough_args(next_args, next_kwargs):
-        current_arg_count = len(next_args) + len(next_kwargs)
-        return current_arg_count == get_target_arg_count(func)
 
     @wraps(func)
     def curried(*new_args, **new_kwargs):
@@ -21,11 +18,16 @@ def curry(func, args=None, kwargs=None):
         next_kwargs = kwargs.copy()
         next_kwargs.update(new_kwargs)
 
-        if have_enough_args(next_args, next_kwargs):
+        if have_enough_args(func, next_args, next_kwargs):
             return func(*next_args, **next_kwargs)
         return curry(func, args=next_args, kwargs=next_kwargs)
     
     return curried
+
+
+def have_enough_args(func, next_args, next_kwargs):
+    current_arg_count = len(next_args) + len(next_kwargs)
+    return current_arg_count == get_target_arg_count(func)
 
 
 def get_target_arg_count(func):
